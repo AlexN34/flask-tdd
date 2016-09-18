251e69932091de3d390e75d43365f61df22fcaa7
AlexN34 <alex-nguyen@live.com.au>
2016-09-18 23:34:07 +1000

    More tests

--- app-test.py
+++ app-test.py
@@ -3,29 +3,48 @@ from app import app
 import MySQLdb
 import unittest
 
+
 class BasicTestCase(unittest.TestCase):
     server = 'localhost'
     user = 'root'
     password = 'cookies'
+
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 404)
 
     def test_database(self):
+        dbName = 'flaskTest'
         conn = MySQLdb.Connect(self.server, self.user, self.password)
         cursor = conn.cursor()
-        table = "university"
+        cursor.execute("USE %s" % (dbName))
+        """
+        Database should contain a User, Book, Book_List, University
+        Table
+        TODO: Expand table tests for fields to look for
+        Possible alternative:
+        select max(table_catalog) as x from information_schema.tables
+
+        """
+        reqTables = ["User", "Book", "Book_List", "University"]
         try:
-            cursor.execute("""
-            SELECT NULL FROM %d
-            """ % (table)
-        except MySQLdb.DatabaseError:
-            print("ERROR %d IN DATABASE: %s" % (e.args[0], e.args[1]))
-        except MySQLdb.Error:
+            for table in reqTables:
+                cursor.execute(" SELECT 1 FROM %s" % (table))
+                result = cursor.fetchone()
+                # self.assertTrue(result, "Empty result from call, \
+                # table: %s" % (table))
+                # Use below test for now to pass; swap for above test later
+                self.assertFalse(result, "Nonempty result from null call, \
+                                table: %s" % (table))
+
+        except MySQLdb.Error as e:
             print("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
+        except MySQLdb.DatabaseError as e:
+            print("ERROR %d IN DATABASE: %s" % (e.args[0], e.args[1]))
         finally:
             # test there is a database at localhost
+            print("end of database tests")
 
 if __name__ == '__main__':
     unittest.main()
--- app.py
+++ app.py
@@ -2,19 +2,33 @@
 # imports
 from flask import Flask, request, session, g, redirect, \
     url_for, abort, render_template, flash, jsonify
+import MySQLdb
 
-#configuration of db 
+# configuration of db
+HOST = 'localhost'
 USERNAME = 'root'
 PASSWORD = 'cookies'
 
 
-#create and initialise app
+# create and initialise app
 app = Flask(__name__)
 app.config.from_object(__name__)
-
 # @app.route("/")
 # def hello():
 #     return "Hello, World!"
 
+# Connect to database
+
+
+def connect_db():
+    """ Connects to the Database. """
+    conn = MySQLdb.Connect(HOST, USERNAME, PASSWORD)
+    return conn
+
+# @app.teardown_appcontext
+# def close_db(error):
+    # if connection is available, close it down
+
 if __name__ == "__main__":
+    connect_db()
     app.run()
